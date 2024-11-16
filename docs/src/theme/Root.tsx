41089597371e1df16f611a96535dd704c21df7f9
SedationH <sedationh@gmail.com>
2024-11-16 11:16:27 +0800

    feat: add PeterCat AI Assistant integration

---
+++ docs/src/theme/Root.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Assistant } from "@petercatai/assistant";
+import "@petercatai/assistant/style";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
+const PeterCatAssistant = ({ token }: { token: string }) => (
+  <Assistant token={token} showBubble={true} isVisible={false} />
+);
+
+export default function Root({ children }) {
+  const {
+    siteConfig: { customFields },
+  } = useDocusaurusContext();
+  const token = customFields.peterCatAssistantToken as string;
+
+  return (
+    <>
+      <PeterCatAssistant token={token} />
+      {children}
+    </>
+  );
+}
