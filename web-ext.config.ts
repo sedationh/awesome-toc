2bbc8248358b61ab1a496233bdd20e7766cacfdf
SedationH <sedationh@gmail.com>
2024-11-29 08:23:03 +0800

    🔧(release): Update release workflow and web-ext config

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -3,6 +3,6 @@ import { defineRunnerConfig } from "wxt";
 export default defineRunnerConfig({
   startUrls: [
     "https://wuxinhua.com/posts/Dive-Into-Docker/",
-    "https://mp.weixin.qq.com/s?__biz=MjM5MTA1MjAxMQ==&mid=2651264685&idx=1&sn=abd482257802b379e17fd5175e11d9c6&chksm=bd48cf298a3f463f411f6f2f785b20c921c30d12b9bf90763ca01bf98ca36fe741f8a4cefd6e#rd",
+    "https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Video_and_audio_content",
   ],
 });
